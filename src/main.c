6aca7432243c0f571a7541c4a7bdb19ea1181bde
Yupeng Li <yupengli@seas.upenn.edu>
2019-10-19 00:12:46 -0400

    Final code for 3.2 to drive two motors

--- src/main.c
+++ src/main.c
@@ -11,37 +11,36 @@
 void adc_setup(int ADCchannel);
 void adc_read();
 static int adcout=0;
-
-int adc1 = 12;
-int adc2 = 11;
-int adc;
+int adc = 11;
 int main(void)
 {
   m_usb_init();
-  set(ADMUX,REFS0);
-  teensy_clockdivide(0);
-  set(ADCSRA,ADPS0);
-  set(ADCSRA,ADPS1);
-  set(ADCSRA,ADPS2);
-  set(DDRC,6);
   set(DDRB,7);
   set(TCCR1B,CS10);
   set(TCCR1B,CS12);
   set(TCCR1A,WGM10);
   set(TCCR1B,WGM12);
   set(TCCR1A,COM1C1);
+  set(ADMUX,REFS0);
+  teensy_clockdivide(0);
+  set(ADCSRA,ADPS0);
+  set(ADCSRA,ADPS1);
+  set(ADCSRA,ADPS2);
+  set(DDRC,6);
   set(TCCR3B,CS30);
   set(TCCR3B,CS32);
   set(TCCR3A,WGM30);
   set(TCCR3B,WGM32);
   set(TCCR3A,COM3A1);
-  adc = adc1;
+
+
 
 
   for(;;)
   {
-    adc_setup(adc);
+    adc_setup(11);
     adc_read();
+
     // if (adc == adc1)
     // {
     //   adc = adc2;
@@ -50,7 +49,11 @@ int main(void)
     // {
     //   adc = adc1;
     // }
-     OCR3A = 40-(adcout*(40-7))/1024;
+     OCR3A = 40-(adcout*(40-7))/682;
+     adc_setup(12);
+     adc_read();
+     OCR1C = 40-(adcout*(40-7))/682;
+
 
   }
 
@@ -62,6 +65,7 @@ void adc_setup(int ADCchannel)
 
   if (ADCchannel == 1)
   {
+
     set(DIDR0,ADC1D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX0);
@@ -71,6 +75,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 4)
   {
+
     set(DIDR0,ADC4D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX2);
@@ -80,6 +85,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 5)
   {
+
     set(DIDR0,ADC5D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX2);
@@ -89,6 +95,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 6)
   {
+
     set(DIDR0,ADC6D);
     set(ADCSRA,ADATE);
     clear(ADMUX,MUX0);
@@ -98,6 +105,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 7)
   {
+
     set(DIDR0,ADC7D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX0);
@@ -107,6 +115,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 8)
   {
+
     set(DIDR2,ADC8D);
     set(ADCSRA,ADATE);
     clear(ADMUX,MUX0);
@@ -116,6 +125,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 9)
   {
+
     set(DIDR2,ADC9D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX0);
@@ -125,6 +135,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 10)
   {
+
     set(DIDR2,ADC10D);
     set(ADCSRA,ADATE);
     clear(ADMUX,MUX0);
@@ -134,6 +145,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 11)
   {
+
     set(DIDR2,ADC11D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX0);
@@ -144,6 +156,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 12)
   {
+
     set(DIDR2,ADC12D);
     set(ADCSRA,ADATE);
     clear(ADMUX,MUX0);
@@ -153,6 +166,7 @@ void adc_setup(int ADCchannel)
   }
   if (ADCchannel == 13)
   {
+
     set(DIDR2,ADC13D);
     set(ADCSRA,ADATE);
     set(ADMUX,MUX0);
@@ -160,13 +174,13 @@ void adc_setup(int ADCchannel)
     set(ADMUX,MUX2);
     set(ADCSRB,MUX5);
   }
+
 }
 void adc_read()
 {
   set(ADCSRA,ADEN);
   set(ADCSRA,ADSC);
-  if (bit_is_set(ADCSRA,ADIF))
-  {
+  while (!bit_is_set(ADCSRA,ADIF));
     set(ADCSRA,ADIF);
     m_usb_tx_string("\nADC");
     m_usb_tx_uint(adc);
@@ -174,5 +188,4 @@ void adc_read()
     m_usb_tx_uint(ADC*5/1.024);
     adcout= ADC;
     m_usb_tx_string("mV");
-  }
 }
